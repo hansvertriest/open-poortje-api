3abc44af362a6a85aa91d69249843c6be7f5b91e
Hans Vertriest <hans.vertriest@gmail.com>
2021-01-12 11:12:12 +0100

    enalbed cors

--- src/middleware/GlobalMiddleware.ts
+++ src/middleware/GlobalMiddleware.ts
@@ -1,9 +1,16 @@
 import { Application } from 'express';
 import * as bodyParser from 'body-parser';
+import { default as cors } from 'cors';
 
 class GlobalMiddleware {
     public static load(app: Application) {
-        app.use(bodyParser.json({ limit: '50mb' }))
+        app.use(bodyParser.json({ limit: '50mb' }));
+        app.use(cors({
+            origin: true,
+            methods: 'GET, HEAD, PUT,PATCH,POST,DELETE',
+            credentials: true,
+            exposedHeaders: ['x-auth-token'],
+        }));
     }
 
 }
